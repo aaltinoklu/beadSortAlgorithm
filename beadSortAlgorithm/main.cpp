99dd5a4b4196ccbfbf0d6ad248f64916dcb4221d
Aşkın Altınoklu <altinokluaskin@gmail.com>
2022-07-08 01:36:57 +0300

    beadSort function is created. created Function is tested in main and validated with sort function of "stl".

--- beadSortAlgorithm/main.cpp
+++ beadSortAlgorithm/main.cpp
@@ -2,17 +2,23 @@
 //  main.cpp
 //  beadSortAlgorithm
 //
-//  Created by Askin on 21.06.2022.
+//  Created by aaltinoklu on 21.06.2022.
 //
+//  Last Modified by aaltinoklu on 08.07.2022.
+//
+
 
 #include <iostream>
 #include <vector>
+#include <chrono>
+
+
 using namespace std;
 template < class T >
 void printVector(T a){
     
     for (int i=0; i< a.size() ; i++ ) {
-        cout << a[i] << '\n';
+        cout << a[i] << " ";
     }
     
 }
@@ -40,11 +46,11 @@ vector<int>  sumVector2D(vector<vector<int>> a) {
     return sumValue;
 }
 
-void beadSort(vector<int>& a) {
+vector<int> beadSort(vector<int> a) {
     
     //vector<int> a= {100,150,32,45,55,69,125,90,57,85};
     vector<int> b(a.size(),0);
-    
+    vector<int> sortedVec;
     //printVector(b);
     
     int temp;
@@ -76,11 +82,11 @@ void beadSort(vector<int>& a) {
       }
     vector<int> sumValue=sumVector2D(vec);
     
-    cout << "Before Sorting: ";
-    for (int i = 0; i < sumValue.size(); i++) {
-        cout << sumValue[i] << " ";
-    }
-    cout << endl;
+    //cout << "Before Sorting: ";
+    //for (int i = 0; i < sumValue.size(); i++) {
+    //    cout << sumValue[i] << " ";
+    //}
+    //cout << endl;
     
     //int temp;
     for (int i = 0; i < vec.size()-1; i++){
@@ -100,32 +106,60 @@ void beadSort(vector<int>& a) {
             //cout << endl;
         }
         vec=vec2;
-        cout << "Iteration " << i << ":  ";
-        a=sumVector2D(vec);
-        for (int i = 0; i < a.size(); i++) {
-            cout << a[i] << " ";
-        }
-        cout << endl;
+        sortedVec=sumVector2D(vec);
+        //cout << "Iteration " << i << ":  ";
+        //sumValue=sumVector2D(vec);
+        //for (int i = 0; i < sumValue.size(); i++) {
+        //    cout << sumValue[i] << " ";
+        //}
+        //cout << endl;
         
         
     }
     
-    
+    return sortedVec;
 }
 
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    vector<int> a= {100,150,32,45,55,69,125,90,57,85};
+    //vector<int> a= {100,150,32,45,55,69,125,90,57,85};
+    
+    // Generate vector involving random numbers in a range [RandMin RandMax]
+    int size = 100;
+    vector<int> a(size, 0);
+    srand(time(NULL));
+    generate(a.begin(), a.end(), rand);
+    int RandMin= 0;
+    int RandMax= 1000;
+    for (int i=0; i<a.size(); i++) {
+        a[i]=RandMin+a[i]%RandMax;
+    }
+    
+    // Test Algorithms with Created Random Vector
+    
     cout << "Before Sorting" << endl;
     printVector(a);
-    cout << "After Sorting" <<endl;
-    beadSort(a);
+    cout << endl;
+    cout << "After Sorting" << endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    vector<int> a_sorted=beadSort(a);
+    auto finish = std::chrono::high_resolution_clock::now();
+    printVector(a_sorted);
+    cout << endl;
+    chrono::duration<double> elapsed = finish - start;
+    cout << "Elapsed time: " << elapsed.count() << " s\n";
+    
+    // Testing In-house implemented algorithm with a build-in one
+    start = std::chrono::high_resolution_clock::now();
+    sort(a.begin(), a.end(), greater<int>());
+    finish = std::chrono::high_resolution_clock::now();
+    cout << "After Sorting with stl sort function" << endl;
     printVector(a);
-    
-    
-    
+    cout << endl;
+    elapsed = finish - start;
+    cout << "Elapsed time: " << elapsed.count() << " s\n";
     
     
     return 0;
